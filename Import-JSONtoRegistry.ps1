48e5f988ca37ada75dd1a66a5402a4b74b0dcf83
Alexander Kochetkov <a.v.kochetkov@outlook.com>
2020-10-15 17:56:09 +0300

    Solve issue with slash in registry key name

--- Import-JSONtoRegistry.ps1
+++ Import-JSONtoRegistry.ps1
@@ -1,27 +1,39 @@
 function Import-JSONtoRegistry ($JSON, $RegistryPath) {
     foreach ($Key in $JSON.Keys) {
+        try {
+            if (-not (Test-Path "$RegistryPath\$Key")) {
+                New-Item -Path "$RegistryPath\$Key" `
+                    -Type Directory `
+                    -Force `
+                    -ErrorAction Stop | Out-Null
+            }
+        }
+        catch {
+            return $false
+        }
+
         foreach ($SubKey in $JSON[$Key].Keys) {
+            try {
+                $RegItem = Get-Item $RegistryPath
+                $RegKey = $RegItem.OpenSubKey("$Key",$true)
+                $RegKey.CreateSubKey($SubKey)
+            }
+            catch {
+                return $false
+            }
             foreach ($Parameter in $JSON[$Key][$SubKey].Keys) {
                 $Type = $JSON[$Key][$SubKey][$Parameter]["Type"]
                 $Value = $JSON[$Key][$SubKey][$Parameter]["Value"]
                 try {
-                    if (!(Test-Path "$RegistryPath\$Key\$SubKey")) {
-                        New-Item -Path "$RegistryPath\$Key\$SubKey" `
-                            -Type Directory `
-                            -Force `
-                            -ErrorAction Stop | Out-Null
-                    }
                     New-ItemProperty -Path "$RegistryPath\$Key\$SubKey"  `
                         -Name $Parameter `
                         -PropertyType $Type `
                         -Value $Value `
                         -Force `
                         -ErrorAction Stop | Out-Null
-
                 }
                 catch {
-                    Write-Warning "Unable to update registry"
-                    return
+                    return $false
                 }
             }
         }
